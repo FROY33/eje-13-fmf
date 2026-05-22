d5f0a581ce60010de274afa74cb74860cbe835f0
froy33 <f90ymtz@gmail.com>
2026-05-22 09:52:22 -0600

    routes y componente notfound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/auth/login/login';
+import { Registro } from './components/auth/registro/registro';
+import { Forgot } from './components/auth/forgot/forgot';
+import { Notfound } from './components/pages/notfound/notfound';
 
-export const routes: Routes = [];
+//Arreglo de rutas, cada ruta es un objeto con dos propiedades: path y component
+export const routes: Routes = [
+    {
+        //url
+        path: 'login',
+        component:Login
+    },
+    {
+        path: 'registro',
+        component:Registro
+    },
+    {
+        path: 'recuperar',
+        component:Forgot
+    },
+    {
+        path: '**', //cualquier ruta que no exista, redirige a login
+        component:Notfound
+    }
+
+];
