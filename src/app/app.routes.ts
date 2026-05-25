8e65e9486cf9e90366f9f21d45e7900b01029a3e
froy33 <f90ymtz@gmail.com>
2026-05-25 12:01:09 -0600

    resto de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,37 @@ import { Login } from './components/auth/login/login';
 import { Registro } from './components/auth/registro/registro';
 import { Forgot } from './components/auth/forgot/forgot';
 import { Notfound } from './components/pages/notfound/notfound';
+import { Layout } from './components/layout/layout';
+import { Dash } from './components/pages/dash/dash';
+import { Kardex } from './components/pages/kardex/kardex';
+import { Materias } from './components/pages/materias/materias';
+import { Profesores } from './components/pages/profesores/profesores';
 
 //Arreglo de rutas, cada ruta es un objeto con dos propiedades: path y component
 export const routes: Routes = [
+    {
+        path:'layout',
+        component:Layout,
+        children:[
+            {
+                path:'dash',
+                component:Dash
+            },
+            {
+                path:'kardex',
+                component:Kardex
+            },
+            {
+                path:'materias',
+                component:Materias
+            },
+            {
+                path:'profesores',
+                component:Profesores
+            }
+        ]
+
+    },
     {
         //url
         path: 'login',
